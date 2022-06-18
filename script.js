dab1a54aeaad116a3273f3c27c63ffb8bffcbe08
kier2 <54880113+kier2@users.noreply.github.com>
2022-06-18 22:57:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const startBtn = document.querySelector('#start-btn')
 const slideSections = document.querySelectorAll('.slide-section')
-const lastMsg = document.querySelector('.last-msg')
+const lastMsgDivs = document.querySelectorAll('.last-msg h1 > div')
 
 startBtn.addEventListener('click', () => {
     startBtn.parentElement.style.display = 'none'
     slideSections.forEach(element => {
         element.classList.add('active')
     })
-    lastMsg.classList.add('active')
+    lastMsgDivs.forEach(element => {
+        element.classList.add('active')
+    })
+    
 })
